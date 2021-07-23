84e679d317ddac2a1184dd32d41e2de03cef909d
kaizi2009 <kaizi_2009@126.com>
2021-07-23 21:13:28 +0800

    服务发现重写

--- example-discovery-group/example-nacos-discovery-provider/src/main/java/devhelper/cloud/example/ProviderGroupAApplication.java
+++ example-discovery-group/example-nacos-discovery-provider/src/main/java/devhelper/cloud/example/ProviderGroupAApplication.java
@@ -1,5 +1,6 @@
 package devhelper.cloud.example;
 
+import devhelper.cloud.DevHelperCloudDiscoveryConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ProviderGroupAApplication {
 
     public static void main(String[] args) {
+        String prefixGroup = DevHelperCloudDiscoveryConstant.PREFIX_GROUP;
         System.setProperty("server.port", "18082");
         System.setProperty("spring.profiles.active", "group");
         SpringApplication.run(ProviderGroupAApplication.class, args);
